6c7fd1ac0b1211bf9b286624b167256b89abe2b3
adrbessy <adrienbessy@hotmail.com>
2021-03-04 12:34:36 +0100

    update put userAccount

--- src/test/java/com/PayMyBuddy/controller/UserAccountControllerTest.java
+++ src/test/java/com/PayMyBuddy/controller/UserAccountControllerTest.java
@@ -26,6 +26,7 @@ public class UserAccountControllerTest {
   private UserAccountService userAccountService;
 
   private UserAccount userAccount;
+  private UserAccount userAccount2;
 
   @Test
   public void testGetUserAccounts() throws Exception {
@@ -35,6 +36,8 @@ public class UserAccountControllerTest {
   @Test
   public void testCreateUserAccount() throws Exception {
     userAccount = new UserAccount();
+    userAccount.setEmailAddress("adrien@mail.fr");
+    userAccount.setPassword("abcde");
     userAccount.setFirstName("Adrien");
     userAccount.setName("Bessy");
 
@@ -43,6 +46,28 @@ public class UserAccountControllerTest {
     MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/userAccount")
         .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
         .content(new ObjectMapper().writeValueAsString(userAccount));
+
+    this.mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  public void testEditUserAccount() throws Exception {
+    userAccount = new UserAccount();
+    userAccount.setEmailAddress("adrien@mail.fr");
+    userAccount.setPassword("abcde");
+    userAccount.setFirstName("Adrien");
+    userAccount.setName("Bessy");
+    userAccount2 = new UserAccount();
+    userAccount2.setEmailAddress("adrien@mail.fr");
+    userAccount2.setPassword("xyz");
+
+    when(userAccountService.userAccountEmailExist("adrien@mail.fr")).thenReturn(true);
+    when(userAccountService.getUserAccount("adrien@mail.fr")).thenReturn(userAccount);
+    when(userAccountService.saveUserAccount(userAccount)).thenReturn(userAccount);
+
+    MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/userAccount/adrien@mail.fr")
+        .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
+        .content(new ObjectMapper().writeValueAsString(userAccount2));
 
     this.mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
   }
