a81e2e34f0f66ae2a4be57f309385bfc6013c409
adrbessy <adrienbessy@hotmail.com>
2021-03-26 18:01:46 +0100

    update tests

--- src/test/java/com/PayMyBuddy/controller/HomeRessourceControllerTest.java
+++ src/test/java/com/PayMyBuddy/controller/HomeRessourceControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import com.PayMyBuddy.model.BankAccount;
 import com.PayMyBuddy.model.TransactionDto;
 import com.PayMyBuddy.model.UserAccount;
 import com.PayMyBuddy.model.UserAccountDto;
@@ -26,6 +27,9 @@ public class HomeRessourceControllerTest {
 
   @MockBean
   private UserAccountController userAccountControllerMock;
+
+  @MockBean
+  private BankAccountController bankAccountControllerMock;
 
   @MockBean
   private FriendController friendControllerMock;
@@ -53,11 +57,68 @@ public class HomeRessourceControllerTest {
     userAccount.setPassword("abcde");
     userAccount.setFirstName("Adrien");
     userAccount.setName("Bessy");
+
     when(userAccountControllerMock.getMyUserAccount("adrien@mail.fr"))
         .thenReturn(userAccount);
+
     mockMvc.perform(get("/user"))
         .andExpect(status().isOk()).andExpect(view().name("user"));
   }
+
+  @Test
+  @WithMockUser(username = "adrien@mail.fr")
+  public void testProfile() throws Exception {
+    List<BankAccount> myBankAccounts = new ArrayList<>();
+
+    when(bankAccountControllerMock.getMyBankAccounts("adrien@mail.fr"))
+        .thenReturn(myBankAccounts);
+
+    mockMvc.perform(get("/profile"))
+        .andExpect(status().isOk()).andExpect(view().name("profile"));
+  }
+
+  /*
+   * @Test
+   * 
+   * @WithMockUser(username = "adrien@mail.fr") public void
+   * testDeleteBankAccount() throws Exception { BankAccount bankAccount = new
+   * BankAccount();
+   * 
+   * when(bankAccountControllerMock.deleteBankAccount("adrien@mail.fr",
+   * "FR3546565449854")).thenReturn(bankAccount);
+   * 
+   * mockMvc.perform(get(
+   * "/deleteBankAccount?emailAddress=adrien@mail.fr&iban=FR3546565449854"))
+   * .andExpect(status().isOk()).andExpect(view().name("profile")); }
+   */
+
+  /*
+   * @Test
+   * 
+   * @WithMockUser(username = "adrien@mail.fr") public void testAdminUser() throws
+   * Exception { userAccount = new UserAccount();
+   * userAccount.setEmailAddress("adrien@mail.fr");
+   * userAccount.setPassword("abcde"); userAccount.setFirstName("Adrien");
+   * userAccount.setName("Bessy"); List<UserAccount> userAccountList = new
+   * ArrayList<>(); userAccountList.add(userAccount); List<TransactionDto>
+   * transactionDtoList = new ArrayList<>(); TransactionDto transactionDto = new
+   * TransactionDto("connection", "description", "amount", "myDate");
+   * transactionDtoList.add(transactionDto);
+   * 
+   * when(userAccountControllerMock.getMyUserAccount("adrien@mail.fr"))
+   * .thenReturn(userAccount); when(userAccountControllerMock.getUserAccounts())
+   * .thenReturn(userAccountList);
+   * 
+   * // mockMvc.perform(get("/admin_user", //
+   * transactionDtoList)).andExpect(status().isOk()).andExpect(view().name(
+   * "admin_user"));
+   * 
+   * MockHttpServletRequestBuilder builder =
+   * MockMvcRequestBuilders.post("/admin_user").flashAttr("transactionList",
+   * transactionDtoList);
+   * this.mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk()
+   * ).andExpect(view().name("admin_user")); }
+   */
 
   @Test
   @WithMockUser(username = "adrien@mail.fr")
@@ -95,6 +156,28 @@ public class HomeRessourceControllerTest {
     mockMvc.perform(get("/transac"))
         .andExpect(status().isOk()).andExpect(view().name("transaction"));
   }
+
+  /*
+   * @Test
+   * 
+   * @WithMockUser(roles = "ADMIN") public void testDeposit() throws Exception {
+   * userAccount = new UserAccount();
+   * userAccount.setEmailAddress("adrien@mail.fr");
+   * userAccount.setPassword("abcde"); userAccount.setFirstName("Adrien");
+   * userAccount.setName("Bessy"); double amount = 800.0;
+   * userAccount.setAmount(amount); transactionDto = new
+   * TransactionDto("isabelle@mail.fr", "for a present", "50", "");
+   * List<TransactionDto> transactionList = new ArrayList<>();
+   * transactionList.add(transactionDto); Transaction transaction = new
+   * Transaction(); transaction.setEmailAddressReceiver("adrien@mail.fr");
+   * 
+   * when(transactionControllerMock.getMyTransactions("adrien@mail.fr"))
+   * .thenReturn(transactionList);
+   * 
+   * MockHttpServletRequestBuilder builder =
+   * MockMvcRequestBuilders.post("/moneyDeposit").flashAttr("newTransaction",
+   * transaction); mockMvc.perform(builder) .andExpect(status().isOk()); }
+   */
 
   @Test
   @WithMockUser(roles = "ADMIN")
